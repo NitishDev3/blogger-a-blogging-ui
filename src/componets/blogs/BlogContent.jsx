f991fdf5069962807a1832a1fb7ce7ec55b050a8
nitishdev3 <nitishsahunisi3@gmail.com>
2025-02-22 20:26:03 +0530

    Complete App

--- src/componets/blogs/BlogContent.jsx
+++ src/componets/blogs/BlogContent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Blog.css";
+import "./blog.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { deleteBlog } from "../../store/blogSlice";
